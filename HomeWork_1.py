dc2e5879b6df22e646c5c9229bfe8c6da637f275
erkinovelzat9-hub <erkinovelzat9@gmail.com>
2026-04-07 01:20:00 +0600

    Moved file from venv to root

---
+++ HomeWork_1.py
@@ -0,0 +1,43 @@
+class Hero:
+
+
+    def __init__(self, name, level, health, strength):
+        self.name = name
+        self.level = level
+        self.health = health
+        self.strength = strength
+
+
+    def greet(self):
+        print(f"Привет, я {self.name}, мой уровень: {self.level}")
+
+
+    def attack(self):
+        print(f"{self.name} наносит удар!")
+        self.strength -= 1
+        print(f"сила героя: {self.strength}")
+
+
+
+    def rest(self):
+        print(f"{self.name} отдыхает..")
+        self.health += 1
+        print(f"Здоровье героя: {self.health}\n")
+
+# Первый объект
+Kirito = Hero("Кирито", 50, 100, 500)
+print(f"Имя: {Kirito.name} \nУровень: {Kirito.level} \nЗдоровья: {Kirito.health}, "
+      f"\nсила: {Kirito.strength}")
+Kirito.greet()
+Kirito.attack()
+Kirito.rest()
+
+
+Haruna = Hero("Харуна", 10, 50, 100 )
+print(f"Имя: {Haruna.name} \nУровень: {Haruna.level} \nЗдоровья: {Haruna.health}, "
+      f"\nсила: {Haruna.strength}")
+Haruna.greet()
+Haruna.attack()
+Haruna.rest()
+
+
